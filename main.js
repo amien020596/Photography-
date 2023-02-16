49c22fc66fb377dd539de16d715fbe334ccf3408
Amien kurniawan <offdevamienk@gmail.com>
2023-02-16 23:06:16 +0700

    update header

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// change nav style on scroll
 window.addEventListener('scroll', () => {
   document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0)
+})
+
+// contact button (curcular text)
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach(textButton => {
+  let text = textButton.querySelector('p');
+
+  text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform:rotate(${index * 12}deg)">${character}</span>`).join('')
 })
