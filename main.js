bdc1bcaf693d8f5df7f5c77f94b7e52f359ea190
Amien kurniawan <offdevamienk@gmail.com>
2023-02-18 22:25:57 +0700

    tablet responsive

--- main.js
+++ main.js
@@ -33,3 +33,26 @@ var swiper = new Swiper(".mySwiper", {
   }
 });
 
+
+// NAV
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector('#nav__toggle-open');
+const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+const openNav = () => {
+  nav.style.display = 'flex';
+  openNavBtn.style.display = 'none';
+  closeNavBtn.style.display = 'inline-block';
+}
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () => {
+  nav.style.display = 'none';
+  openNavBtn.style.display = 'inline-block';
+  closeNavBtn.style.display = 'none';
+}
+closeNavBtn.addEventListener('click', closeNav);
+
+nav.querySelectorAll('li a').forEach(navLink => {
+  navLink.addEventListener('click', closeNav);
+})
